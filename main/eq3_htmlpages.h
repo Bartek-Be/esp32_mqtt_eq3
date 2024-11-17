3c1de0b3cbcb70e5661b9aa0065092c123930707
Bartek-Be <bartlomiej.bialek@gmail.com>
2024-11-17 22:46:25 +0100

    update html pages

--- main/eq3_htmlpages.h
+++ main/eq3_htmlpages.h
@@ -22,12 +22,12 @@ const char pagefooter_a[] = R"EOF(
 </tbody>
 </table>
 <div style="font-size:12px;"><hr/>
-    <a href="https://github.com/softypit/esp32_mqtt_eq3" target="_blank" style="color:#aaa;">EQ3-MQTT-ESP32)EOF";
+    <a href="https://github.com/softypit/esp32_mqtt_eq3" target="_blank" style="color: #AAA;">EQ3-MQTT-ESP32)EOF";
 
 const char pagefooter_b[] = " "EQ3_MAJVER"."EQ3_MINVER""EQ3_EXTRAVER" ";
 
 const char pagefooter_c[] = R"EOF(by SoftyPIT</a><br/>
-    <a href="https://github.com/Bartek-Be/esp32_mqtt_eq3" target="_blank" style="color:#aaa;">fork</a></div>
+    <a href="https://github.com/Bartek-Be/esp32_mqtt_eq3" target="_blank" style="color: #AAA;">fork</a></div>
 </div>
 
 </body>
