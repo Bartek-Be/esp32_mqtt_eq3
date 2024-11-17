e7b36fe65bc792780cdc3d21d61dc6537140bbaf
Bartek-Be <bartlomiej.bialek@gmail.com>
2024-11-17 22:24:19 +0100

    update html pages

--- main/eq3_htmlpages.h
+++ main/eq3_htmlpages.h
@@ -2,10 +2,11 @@
 const char pageheader[] = R"EOF(
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no"/>
-        <title>EQ3 status</title>
-    </head>
+<head>
+    <meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no"/>
+    <title>EQ3 status</title>
+</head>
+
 <body>
 <div style="text-align:center;">
 )EOF";
@@ -20,6 +21,7 @@ const char pagefooter_a[] = R"EOF(
     <tr><td><a href="/scan">Rescan for EQ3 devices</a></td><td> | </td><td><a href="/restartnow" style="color: rbg(255,0,0)"><font color="FF0000">Reboot ESP</font></a></td></tr>
 </tbody>
 </table>
+
 <div style="font-size:12px;"><hr/>
     <a href="https://github.com/softypit/esp32_mqtt_eq3" target="_blank" style="color:#aaa;">EQ3-MQTT-ESP32)EOF";
 
@@ -41,26 +43,26 @@ const char pageemptyfooter[] = R"EOF(
 const char selectap[] = R"EOF(
 <h1>Select WiFi</h1>
 <form action="configSubmit" method="post">
-    <table style="margin:1em auto;">
-    <tbody>
-        <tr><td>SSID:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="ssid" value="%s" /></td></tr>
-        <tr><td>Password:</td><td><input type="password" autocorrect="off" autocapitalize="none" name="password" value="%s" placeholder="hidden" /></td></tr>
-        <tr><td>MQTT URL:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="mqtturl" value="%s" /></td></tr>
-        <tr><td>MQTT username:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="mqttuser" value="%s" /></td></tr>
-        <tr><td>MQTT password:</td><td><input type="password" autocorrect="off" autocapitalize="none" name="mqttpass" value="%s" placeholder="hidden" /></td></tr>
-        <tr><td>MQTT ID:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="mqttid" value="%s" /></td></tr>
-        <tr><td>NTP enabled:</td><td><input type="checkbox" name="ntpenabled" value="true" %s/></td></tr>
-        <tr><td>NTP server 1:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="ntpserver1" value="%s" /></td></tr>
-        <tr><td>NTP server 2:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="ntpserver2" value="%s" /></td></tr>
-        <tr><td>Timezone:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="ntptimezone" value="%s" /></td></tr>
-        <tr><td>IP address:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="ip" value="%s" /></td></tr>
-        <tr><td>Gateway address:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="gw" value="%s" /></td></tr>
-        <tr><td>Netmask:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="netmask" value="%s" /></td></tr>
-        <tr><td>DNS server 1:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="dns1ip" value="%s" /></td></tr>
-        <tr><td>DNS server 2:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="dns2ip" value="%s" /></td></tr>
-    </tbody>
-    </table>
-    <div><input type="submit" value="Submit"></div>
+<table style="margin:1em auto;">
+<tbody>
+    <tr><td>SSID:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="ssid" value="%s" /></td></tr>
+    <tr><td>Password:</td><td><input type="password" autocorrect="off" autocapitalize="none" name="password" value="%s" placeholder="hidden" /></td></tr>
+    <tr><td>MQTT URL:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="mqtturl" value="%s" /></td></tr>
+    <tr><td>MQTT username:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="mqttuser" value="%s" /></td></tr>
+    <tr><td>MQTT password:</td><td><input type="password" autocorrect="off" autocapitalize="none" name="mqttpass" value="%s" placeholder="hidden" /></td></tr>
+    <tr><td>MQTT ID:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="mqttid" value="%s" /></td></tr>
+    <tr><td>NTP enabled:</td><td><input type="checkbox" name="ntpenabled" value="true" %s/></td></tr>
+    <tr><td>NTP server 1:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="ntpserver1" value="%s" /></td></tr>
+    <tr><td>NTP server 2:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="ntpserver2" value="%s" /></td></tr>
+    <tr><td>Timezone:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="ntptimezone" value="%s" /></td></tr>
+    <tr><td>IP address:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="ip" value="%s" /></td></tr>
+    <tr><td>Gateway address:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="gw" value="%s" /></td></tr>
+    <tr><td>Netmask:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="netmask" value="%s" /></td></tr>
+    <tr><td>DNS server 1:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="dns1ip" value="%s" /></td></tr>
+    <tr><td>DNS server 2:</td><td><input type="text" autocorrect="off" autocapitalize="none" name="dns2ip" value="%s" /></td></tr>
+</tbody>
+</table>
+<div><input type="submit" value="Submit"></div>
 </form>
 <div>
     The IP address, gateway address and netmask are optional. If not supplied
@@ -122,12 +124,12 @@ const char upload[] = R"EOF(
 <title>upload firmware</title> 
 <div><h1>Upload new software</h1></div>
 <form action="otaupload" method="POST" enctype="multipart/form-data">
-    <table style="margin:1em auto;">
-    <tbody>
-        <tr><td><input type="file" name="fileupload" value="fileupload" id="fileupload"></td></tr>
-        <tr><td><input type="submit" value="Upload and apply"></td></tr>
-    </tbody>
-    </table>
+<table style="margin:1em auto;">
+<tbody>
+    <tr><td><input type="file" name="fileupload" value="fileupload" id="fileupload"></td></tr>
+    <tr><td><input type="submit" value="Upload and apply"></td></tr>
+</tbody>
+</table>
 </form>
 )EOF";
 
@@ -172,13 +174,13 @@ const char command_device_head[] = R"EOF(
 <table style="margin:1em auto;">
 <tbody>
     <tr><td><select name="device">
-            )EOF";
+        )EOF";
 
 const char select_device_entry[] = "<option value=\"%s\">%s</option>";
 
 const char command_post_device[] = R"EOF(
-    </select></td><td>
-    <select name="command">
+    </select></td>
+    <td><select name="command">
         <option value="unlock">unlock</option>
         <option value="lock">lock</option>
         <option value="boost">boost</option>
@@ -207,4 +209,3 @@ const char commanderror[] = R"EOF(
 <title>Invalid command</title> 
 <div><h1>Invalid command</h1></div>
 )EOF";
-
