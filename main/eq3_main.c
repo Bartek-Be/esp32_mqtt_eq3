91733ed2453f0c1cea3d992395c20611f15d6dda
Bartek-Be <bartlomiej.bialek@gmail.com>
2024-11-20 18:36:30 +0100

    back to master

--- main/eq3_main.c
+++ main/eq3_main.c
@@ -1128,37 +1128,12 @@ void confparms(char *mqtturl, char *mqttuser, char *mqttpass, char *mqttid){
         url = NULL;
     }
     if(mqtturl != NULL && mqtturl[0] != 0){
-        // TODO: sprawdzania i dodawanie def portu dla mqtt
-        if(strstr(mqtturl, ":") != NULL){
-            for (int i = 0; i < strlen(mqtturl); i++) {
-                //":" char
-                if(mqtturl[i] == 72){
-                    count++;
-                }
-            }
-            if(count >= 2){
-                url = strdup(mqtturl);
-            }else{
-                if(strstr(mqtturl, "//") != NULL){
-                    url = malloc(strlen(mqtturl) + 6);
-                    sprintf(url, "%s:1883", mqtturl);
-                }else{
-                    url = malloc(strlen(mqtturl) + 8);
-                    sprintf(url, "mqtt://%s", mqtturl);
-                }
-            }
-        }else{
-            url = malloc(strlen(mqtturl) + 13);
-            sprintf(url, "mqtt://%s:1883", mqtturl);
-        }
-       /*
         if(strstr(mqtturl, "//") != NULL){
             url = strdup(mqtturl);
         }else{
             url = malloc(strlen(mqtturl) + 8);
             sprintf(url, "mqtt://%s", mqtturl);
         }
-        */
         ESP_LOGI(GATTC_TAG, "MQTT config url is %s", url);
     }
     if(mqttuser != NULL && mqttuser[0] != 0){
